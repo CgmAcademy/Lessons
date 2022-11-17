000d4ccdbb07c12bc6e9df219ec23328ae93942f
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-17 12:45:35 +0100

    Funzione LoadFromTextFile

--- Generics/DataStore.cs
+++ Generics/DataStore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GenericsLesson
@@ -8,7 +9,7 @@ namespace GenericsLesson
     {
         static int index = 4;
         T[] _data = new T[index];
-        T entry = new T(); 
+        static T entry = new T(); 
 
         public DataStore()
         {
@@ -16,6 +17,7 @@ namespace GenericsLesson
         }
         public void AddItem(T item)
         {
+            
             if (_data[_data.Length -1] != null)
             {
                 GetMoreSpace();
@@ -30,7 +32,7 @@ namespace GenericsLesson
             Array.Copy(_data,_nextData, _data.Length);// + 4 [_data]
             _data = _nextData;  
         }
-        public void Print()
+        public static void Print()
         {
             var campi = entry.GetType().GetProperties();
 
@@ -39,7 +41,20 @@ namespace GenericsLesson
                 Console.WriteLine(item.Name);
             }
         }
-        
+        public static void ShowAllM()
+        {
+           
+            var tm = typeof(Person);
+            foreach (var mi in tm.GetMethods())
+            {
+                Console.WriteLine(mi.Name); ;
+                foreach (var p in mi.GetParameters())
+                {
+                    Console.WriteLine($"\t{p.ParameterType.Name}");
+                }
+            }
+        }
+
     } 
 
 
@@ -79,6 +94,28 @@ namespace GenericsLesson
 
             System.IO.File.WriteAllLines(filePath, lines);
 
+        }
+        public static List<T> LoadFromTextFile<T>(string filePath) where T : class, new()
+        {
+            List<T> output = new List<T>(); // Crea una lista nella quale inserirò tutti  gli oggetti di tipo  T che andrò a creare partendo dai dati del CSV
+
+            var lines = System.IO.File.ReadAllLines(filePath).ToList(); // --> Estrago tutte le righe dal file
+            T entry = new T(); // Creo un oggetto vuoto del tipo che ho T, che servirà come variabile d'appoggio da popolare con i dati.  
+            var cols = entry.GetType().GetProperties();// ->> Estrago i nomi delle proprietà dell'oggetto (rappresenteranno  le colonne). 
+
+            //foreach () // Ciclare tutte le righe del file CSV
+            //{
+            // entry = new T(); // Creare un nuova istanza dell'oggetto che verrà usato per essere valorizzato (le proprietà) con  i valori della riga 
+            // var valoriRiga = splitare  i valori della riga del file separati in modo da create un array          
+
+
+            // ciclare ogni singola colonna del file csv in modo da poter fare un confronto con le proprietà dell'oggetto entry                        
+            // --> Controllare che ci sia un Match tra la colonna ,
+            // --> Se c'è il Match, valorizzare quella proprietà dell'oggetto con il valore della [cella] che corrisponde alla [riga/colonna].  
+            /// -->  entry.property.SetValue([oggetto], Convert.ChangeType(valoreCella, entry.property.PropertyType));
+            /// output.Add(entry);
+            // }
+            return output;
         }
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,30 +7,42 @@ namespace GenericsLesson
     {
         static void Main(string[] args)
         {
-            List<Person> people = new List<Person>();
-            List<Account> conti = new List<Account>();
-            people.Add(new() { Name = "Bruno"}); 
-            people.Add(new() { Name = "Marco"});
-            people.Add(new() { Name = "Diego"});
-            people.Add(new() { Name = "Mauro"});
+            //List<Person> people = new List<Person>();
+            //List<Account> conti = new List<Account>();
+            //people.Add(new() { Name = "Bruno"}); 
+            //people.Add(new() { Name = "Marco"});
+            //people.Add(new() { Name = "Diego"});
+            //people.Add(new() { Name = "Mauro"});
 
-            conti.Add(new() { AccountNumber = 65454515 }); 
-            conti.Add(new() { AccountNumber = 76578768 });
-            conti.Add(new() { AccountNumber = 34343454 });
-            conti.Add(new() { AccountNumber = 76865252 });
-            conti.Add(new() { AccountNumber = 13585888 });
+            //conti.Add(new() { AccountNumber = 65454515 }); 
+            //conti.Add(new() { AccountNumber = 76578768 });
+            //conti.Add(new() { AccountNumber = 34343454 });
+            //conti.Add(new() { AccountNumber = 76865252 });
+            //conti.Add(new() { AccountNumber = 13585888 });
 
-            GenericTextFileLogger.saveToFile<Person>(people, @"D:\logs\saveToFile.csv");
+            //GenericTextFileLogger.saveToFile<Person>(people, @"D:\logs\saveToFile.csv");
+
+            DataStore<Person>.ShowAllM();
 
         }
         
     }
     public class Person
     {
-        public string Name { get; set; }
+        public string Name;
     }
     public class Account
     {
-        public int AccountNumber { get; set; }
+        public int AccountNumber;
+
+
+        public void Withdraw()
+        {
+
+        }
+        public void Deposit()
+        {
+
+        }
     }
 }
