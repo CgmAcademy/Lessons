cfcdb2ff4e549da0828bf32b84d2be06e1c7dd4e
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-18 12:58:03 +0100

    Lesson

---
+++ Exception.Lesson/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exception.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Order(8);// Close Program
+                ConfirmOrder();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }          
+        }
+        static void Order(int order)
+        {
+            try
+            {
+                Payment(8);
+               // ConfirmOrder();// non esesguito
+            }            
+            //catch (System.Exception ex)
+            //{
+            //    
+            //}
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Close DB connection");
+                Console.ResetColor();               
+            }
+            // Se inserisco un Catch dopo il finally continua l'esezuzione fino alla fine metodo
+            Console.WriteLine("statement 1");
+            Console.WriteLine("statement 2");
+            Console.WriteLine("statement 3");
+            Console.WriteLine("statement 4");
+           
+        }
+        static int Payment(decimal number)
+        {
+           // decimal result = number / 0; 
+            throw new System.Exception("Problema inserimento ordine element! "); 
+        }
+        static void ConfirmOrder()
+        {
+            Console.WriteLine("Open DB connection...");
+            Console.WriteLine("Order Confirmed");
+        }
+
+
+    }
+}
