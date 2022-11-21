6af400e4feb2543ec77a5766acc847ea74c1f691
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-21 14:03:26 +0100

    Logging

---
+++ MSLogging/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MSLogging
+{
+    public class Program
+    {
+        static ILogger logMyClass;
+        public Program(ILogger logger)
+        {
+            logMyClass = logger;
+        }
+        public static void Main()
+        {
+            var services = new ServiceCollection().AddLogging(logging => logging.AddConsole());
+            services.AddSingleton<MyClass>();//Singleton or transient?!
+            //services.AddSingleton<Program>();//Singleton or transient?!
+            var s = services.BuildServiceProvider();
+            var myclass = s.GetRequiredService<MyClass>();
+            logMyClass = s.GetRequiredService<ILogger<Program>>();   
+            logMyClass.LogCritical("From Program");
+            MyClass.DoSomething();
+        }
+        
+        
+        class MyClass
+        {
+            static ILogger logMyClass;
+            public MyClass(ILogger<MyClass> logger)
+            {
+                logMyClass = logger;
+            }
+            public static void DoSomething()
+            {               
+                logMyClass.LogInformation("From MyClass");
+            }
+        }
+
+    }
+}
