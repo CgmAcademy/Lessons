313d05576049c5112ffb9e4230dee3c396bfd2c1
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-23 19:41:16 +0100

    Project Created

---
+++ Ioc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ioc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
