1e55f05fd745735d4cef974d5629928ad728e9e7
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-24 16:54:18 +0100

    Test

--- LinqToObjects/SelectStatement.cs
+++ LinqToObjects/SelectStatement.cs
@@ -24,41 +24,41 @@ namespace LinqLesson
             new() { Name = "Lodi", Provincia = "Lodi", Regione = "Lombardia", abitanti = 20000 },
             new() { Name = "Rho", Provincia = "Milano", Regione = "Lombardia", abitanti = 80000 }
         };
-      
-       
+
+
         internal static List<string> SelectStatemnt(string Provincia)
         {
             //Provincia == "Milano" 
-            var result =  (from c in comuni
-                           where c.Provincia == Provincia //  3 comuni
-                           select c.Regione) // result 3 record per il campo REGIONE
-                           //.Distinct().ToList();  // Filtra 
+            var result = (from c in comuni
+                          where c.Provincia == Provincia //  3 comuni
+                          select c.Regione) // result 3 record per il campo REGIONE
+                                            //.Distinct().ToList();  // Filtra 
                            .ToList();
-            return result; 
+            return result;
 
         }
         internal static void SelectTupleByRegione(string Provincia)
         {
-            List<(string Regione, int abitanti)> result =  
+            List<(string Regione, int abitanti)> result =
                 (from c in comuni
-                    where c.Provincia == Provincia //  3 comuni
-                    select new { c.Regione, c.abitanti })  // Crea un lista di coppia Iterable 
+                 where c.Provincia == Provincia //  3 comuni
+                 select new { c.Regione, c.abitanti })  // Crea un lista di coppia Iterable 
                       .Select(r => (r.Regione, r.abitanti)).ToList();  // Tipizzato LIST<string,Int>
 
             foreach (var item in result)
             {
                 Console.WriteLine($"{item.Regione} | {item.abitanti}");
-               
+
             }
         }
         internal static void SelectTupleByComune(string Provincia)
         {
-            List<(string Regione, int Abitanti)> result = 
+            List<(string Regione, int Abitanti)> result =
 
-                ( from c in comuni
-                    where c.Provincia == Provincia //  3 comuni
-                    select new { c.Regione, c.abitanti } ).Select(r => (r.Regione, r.abitanti)).ToList();
-                     
+                (from c in comuni
+                 where c.Provincia == Provincia //  3 comuni
+                 select new { c.Regione, c.abitanti }).Select(r => (r.Regione, r.abitanti)).ToList();
+
             foreach (var item in result)
             {
                 Console.WriteLine($"{item.Regione} | {item.Abitanti}");
@@ -69,31 +69,32 @@ namespace LinqLesson
         internal static void GroupTuple(List<(string Regione, int abitanti)> Tuple)
         {
             foreach (var t in Tuple.GroupBy(info => info.Regione).Select(
-                          group => new {
-                                 Group =  group.Key,
-                                 Total =  group.Sum(a=> a.abitanti).ToString(), 
-                                 Count =  group.Count()
-                           })
+                          group => new
+                          {
+                              Group = group.Key,
+                              Total = group.Sum(a => a.abitanti).ToString(),
+                              Count = group.Count()
+                          })
                          .OrderBy(x => x.Group))
             {
                 Console.WriteLine($"Regione : {t.Group}");
                 Console.WriteLine($"N° Abitanti: {t.Total}");
                 Console.WriteLine($"Tot Province: {t.Count}");
 
-               
+
             }
 
-        } 
+        }
         internal static void CreateStructure()
         {
             var ProvinceDict = (from c in comuni
-                              group c by c.Provincia // if Provincia are 5 it will return  5 Groups  of  item
+                                group c by c.Provincia // if Provincia are 5 it will return  5 Groups  of  item
                               into cg  // put the Groups into a where table Key Field  is the "grouped by" Field
-                              orderby cg.Key ascending  // Order by Regione ascendente 
+                                orderby cg.Key ascending  // Order by Regione ascendente 
                                 select new { cg.Key, cg }).ToList()// Create a couple to be able to iterate
                                        .ToDictionary(i => i.Key, i => i.cg.ToList());
 
-           
+
 
 
             var RegionsDict = (from c in comuni  // 10
@@ -103,10 +104,11 @@ namespace LinqLesson
                                select new { rg.Key, rg }).ToList() // Create a couple to be able to iterate
                                              .ToDictionary(i => i.Key, i => i.rg.Select(o => o.Provincia)
                                                  // .Distinct()
-                                                 .ToList()); 
-            
+                                                 .ToList());
+
+            System.Console.WriteLine("Ciao");
 
         }
     }
-    
+
 }
