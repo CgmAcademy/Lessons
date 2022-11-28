98babb8eb2f554c22d0418ac3addbf6a8f621866
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-28 19:28:53 +0100

    fix

--- Ioc/Program.cs
+++ Ioc/Program.cs
@@ -5,64 +5,67 @@ namespace Ioc
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
 
 
             // CONCRETE SERVICE CREATION AND INJECTION
-            TicketCreator client1 = new TicketCreator(); 
-            TicketCreator client2 = new TicketCreator();
-            client1.Purchase(new AirFranceTicket()); // Client is Choosing the Service Provider. 
-            client2.Purchase(new AirFranceTicket()); // Client is Choosing the Service Provider. 
+            TicketCreator client1 = new TicketCreator();
+           // TicketCreator client2 = new TicketCreator();
+            //client1.Purchase(new AirFranceTicket()); // Client is Choosing the Service Provider. 
+          //  client2.Purchase(new AirFranceTicket()); // Client is Choosing the Service Provider. 
 
-
+        
 
             //  SERVICE CREATION AND INJECTION THROUGH THE FACTORY
-            client1.Purchase(EUAirCompanyFactory.Create()); // Client delegates the choice to the Factory Provider
-          
+            client1.Purchase(EUTicketFactory.GetSituation()); // Client delegates the choice to the Factory Provider
+
         }
     }
     public class TicketCreator
     {
         public void Purchase(FlightTicket travelAgency) //  Inject the abstract object to client 
-        {   
+        {
             // Do some conditions 
             travelAgency.CreateTravel();// Create the 
         }
     }
 
-
-    public class EUAirCompanyFactory
+    public class EUTicketFactory//  Inject the concrete object to the TicketCreator 
     {
-        public static FlightTicket Create()
-        {
-            Console.WriteLine("Created by EUAirCompany Factory");
-            return EUCovidRestriction.GetSituation(); 
-        }
-    }
-    public class EUCovidRestriction//  Inject the concrete object to the TicketCreator 
-    {       
         public static FlightTicket GetSituation()
         {
             switch (EuropeanUnion.Positive)
             {
                 case > 50000:
-                    return new AirFranceTicket();
+                    return new RedZone().GetCompany();
                 case > 20000:
                 case < 50000:
-                    return new KlmTicket();
-                   
+                    return new OrangeZone().GetCompany();
+
                 default:
                     return null;
             }
         }
-    } 
-   
-    public class EuropeanUnion
+    }
+    abstract class EUCovidRestriction
     {
-        public static int Positive { get; } = 30000;          
+        public abstract FlightTicket GetCompany();
+    }
+    class RedZone : EUCovidRestriction
+    {
+        public override FlightTicket GetCompany() { return new AirFranceTicket(); }
+    }
+    class OrangeZone : EUCovidRestriction
+    {
+        public override FlightTicket GetCompany() { return new KlmTicket(); }
+    }
+    public class EuropeanUnion
+    {   
+
+        public static int Positive { get; } = 30000;
     }
 
-    
+
     public class FlightTicket
     {
         public FlightTicket()
