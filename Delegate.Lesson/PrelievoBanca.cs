b78071e140dbefa2441f55e463f8b5e721525141
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-11-29 16:00:08 +0100

    Delegate lesson

---
+++ Delegate.Lesson/PrelievoBanca.cs
@@ -0,0 +1,103 @@
+﻿using System.Threading;
+using System.Xml.Linq;
+
+
+namespace Atena.Delegate.Lesson
+{
+    class PrelievoBanca
+    {
+        public class Conto: IContoNazionale
+        {   
+            public string Name { get; set; }    
+            public COUNTRY _cOUNTRY { get; } = COUNTRY.IT;
+
+            public void Withdraw(decimal amount, Conto banca    ) 
+            {
+                System.Console.WriteLine($"Prelievo effettuato per cliente della banca {banca.Name}");
+            }
+            public void Deposit(decimal amount, Conto banca) 
+            {
+                if (banca.Name != this.Name  )
+                {
+                    System.Console.WriteLine($"non è possbile efettuare deposito in una banca diversa dalla tua.");
+                }
+                else
+                {
+                    System.Console.WriteLine($"Versamento effettuato di EURO {amount}");   
+                }
+            }
+            public void FiscalInfo(Conto banca) 
+            {
+
+            }
+            public void IntenationalWithdraw(decimal amount, Conto banca) { }
+
+        }   
+        internal interface IContoNazionale : IContoInternazionale
+        {
+           
+            public void Deposit(decimal amount, Conto banca);
+            public void FiscalInfo(Conto banca);
+        }
+        internal interface IContoInternazionale
+        {
+            public void Withdraw(decimal amount, Conto banca); 
+        }
+        class ATMGermania : IContoNazionale
+        {
+            private COUNTRY _cOUNTRY { get; } = COUNTRY.DE;
+            
+            public void Withdraw(decimal amount, Conto banca)
+            {
+                if(banca._cOUNTRY == _cOUNTRY)
+                {
+                    
+                    Withdraw(amount, banca);
+
+                }
+                else
+                {
+                    var b = (IContoInternazionale) banca;// Castrazione ! 
+                    IntenationalWithdraw(amount, b);
+                }               
+            }
+            
+            public void Deposit(decimal amount, Conto banca) { }
+            public void FiscalInfo( Conto banca) 
+            {
+                if (banca._cOUNTRY != _cOUNTRY)
+                {
+                    System.Console.WriteLine($"Is not possbible to access fiscal data for internatioanle Customer");
+                }
+                
+            }
+
+            // Funzione solo per il prelievo internazionale
+            void IntenationalWithdraw(decimal amount, IContoInternazionale bancaInternazionale)
+            {
+                var b = (Conto) bancaInternazionale;                
+                bancaInternazionale.Withdraw(amount, b);
+            }
+        }
+        class Client
+        {
+            public void Withdraw()
+            {
+                Conto unicredit = new Conto();
+                ATMGermania aTMGermania = new ATMGermania();
+
+                aTMGermania.Withdraw(1000, unicredit);
+            }
+        }
+        public enum COUNTRY
+        {
+            DE,
+            IT,
+            FR,
+
+        }
+    } 
+
+    
+    
+}
---
+++ Delegate.Lesson/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using static Atena.Delegate.Lesson.Program;
+
+namespace Atena.Delegate.Lesson
+{
+    
+    public delegate string EUDigitalWalletAction();
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            EUDigitalWallet personalWallet = new EUDigitalWallet();
+            AssicurazioneSanitaria allianz = new AssicurazioneSanitaria();
+            EUDigitalWalletAction datiSanitari = personalWallet.GetSituazioneSanitaria;
+          //  NotificationAction notificationAction = Notification;
+
+            allianz.Create(datiSanitari); 
+            allianz.Delete(Notification);
+
+        }
+        public static void Notification(string msg) // Callback
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor(); 
+        }
+    } 
+    public class EUDigitalWallet
+    {
+        string _diploma;
+        string _dichiarazioneReddito;
+        string _fedinaPenale; 
+        string _cartellaClinica = "Situazione sanitaria è OK";  
+
+        public string GetIstruzione()
+        {
+            return _diploma;   
+        }
+        public string GetReddito()
+        {
+            return _dichiarazioneReddito;
+        }
+        public string GetSituazionePenale()
+        {
+            return _fedinaPenale;
+        }
+        public string GetSituazioneSanitaria()
+        {
+            return _cartellaClinica;
+        }
+    }
+    public class AssicurazioneSanitaria 
+    {
+       public void Create(EUDigitalWalletAction personalWallet) 
+       {
+            Console.WriteLine(personalWallet());          
+       }
+       public void Delete(Action<string> notificationAction) 
+       {
+            Console.ForegroundColor = ConsoleColor.Red; 
+            notificationAction("Cancellazione eseguita! ");
+            Console.ResetColor(); 
+       }
+    }
+    
+}
