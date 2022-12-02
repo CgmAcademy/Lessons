a32f8dd997860df5e6ddc53ec0af3ec5a8aa46fc
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-12-02 15:12:54 +0100

    IoC +  Abstract Factory lesson

---
+++ AbstractFactory/AbsClass/CovidFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace AbstractFactory.AbsClass
+{
+    
+    abstract class CovidFactory
+    {
+        protected  string _restrictionType;
+        public abstract void Needs();
+    }
+}
---
+++ AbstractFactory/AbsFactory/TravelFactory.cs
@@ -0,0 +1,15 @@
+﻿using AbstractFactory.AbsClass;
+using AbstractFactory.Enums;
+
+namespace AbstractFactory.AbsFactory
+{
+    /// <summary>
+    /// The 'AbstractFactory' abstract class
+    /// </summary>
+
+    abstract class AbstractTravelFactory
+    {
+        public abstract CovidFactory GetRestrictionInfo();
+        public abstract TrainAgencyFactory CreateTravel();
+    }
+}
