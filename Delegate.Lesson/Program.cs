f857873b17d8f01eeb8f829e2497c85c3f968cc4
Bruno Ferreira <b.ferreira.rocha@eatproject365.onmicrosoft.com>
2022-12-02 15:11:49 +0100

    Delegate lesson

--- Delegate.Lesson/Program.cs
+++ Delegate.Lesson/Program.cs
@@ -1,12 +1,14 @@
-﻿
+﻿using Delegate.Lesson;
 using System;
-using static Atena.Delegate.Lesson.Program;
+using System.Collections.Generic;
+using System.Drawing;
 
-namespace Atena.Delegate.Lesson
+namespace Delegate.Lesson
 {
-    
-    public delegate string EUDigitalWalletAction();
 
+    public delegate string EUDigitalWalletAction();
+    public delegate void DelMessage();
+    // public delegate void NotificationAction(string mgs);
     internal class Program
     {
 
@@ -14,26 +16,110 @@ namespace Atena.Delegate.Lesson
         {
             EUDigitalWallet personalWallet = new EUDigitalWallet();
             AssicurazioneSanitaria allianz = new AssicurazioneSanitaria();
-            EUDigitalWalletAction datiSanitari = personalWallet.GetSituazioneSanitaria;
-          //  NotificationAction notificationAction = Notification;
 
-            allianz.Create(datiSanitari); 
-            allianz.Delete(Notification);
 
+            EUDigitalWalletAction multidel, datiSanitari, DatiPenali;
+            datiSanitari = personalWallet.GetSituazioneSanitaria;
+            DatiPenali = personalWallet.GetSituazionePenale;
+
+            //Multidel
+            multidel = DatiPenali ;
+            multidel += datiSanitari;
+
+
+
+            // Anonymous Delegate
+            multidel += ()=> { return "Anonymous Delegate"; };
+
+
+            Action<string> action = (s) => { };
+
+            multidel(); // Eseguo il MultiDelegate
+            Console.WriteLine("---------------------");
+            allianz.Create(multidel); // eseguo tutti le function dendro il delegate
+
+            // Func Delegate return a String 
+            Func<string> getRandomNumber = delegate ()
+            {                 
+               Random rnd = new Random();               
+               return  rnd.Next(1, 100).ToString();
+            };
+
+
+            // Func Delegate return a String  and Accept an 'int' parameter
+            Func<int, string> random = (x) => {
+                Random rnd = new Random();
+                return rnd.Next(1, 100).ToString();
+            };
+
+
+          //  multidel += new EUDigitalWalletAction(getRandomNumber);
+
+
+            // Fucniton delgate with Anonymous Function
+            Func<int, int, string> Sum = (x, y) => (x + y).ToString();
+
+          
+
+
+            TryPredicate();
+
+
+            //  NotificationAction notificationAction = Notification;
+            //  Console.WriteLine(multidel());
+            // Console.WriteLine(multidel());
+        //    allianz.Create(multidel);
+            //allianz.Delete((msg) =>
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Magenta;
+            //    Console.WriteLine(msg);
+            //    Console.ResetColor();
+            //});
+
+
+            
+
+           
+
+
+            //Console.WriteLine(MultiMsg());
         }
         public static void Notification(string msg) // Callback
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(msg);
             Console.ResetColor(); 
+        } 
+        public static void TryPredicate()
+        {
+            Point[] points = { new Point(100, 200),
+                         new Point(150, 250), new Point(250, 375),
+                         new Point(275, 395), new Point(295, 450) };
+
+            // Define the Predicate<T> delegate.
+            Predicate<Point> predicate = FindPoints;
+
+            // Find the first Point structure for which X times Y
+            // is greater than 100000.
+            Point first = Array.Find(points, predicate);
+
+            // Display the first structure found.
+            Console.WriteLine("Found: X = {0}, Y = {1}", first.X, first.Y);
         }
+
+        private static bool FindPoints(Point obj)
+        {
+            return obj.X * obj.Y > 100000;
+        }
+    }
+        
     } 
     public class EUDigitalWallet
     {
         string _diploma;
         string _dichiarazioneReddito;
-        string _fedinaPenale; 
-        string _cartellaClinica = "Situazione sanitaria è OK";  
+        string _fedinaPenale = "Situazione PENALE è OK";
+        string _cartellaClinica = "Situazione SANITARIA è OK";  
 
         public string GetIstruzione()
         {
@@ -56,8 +142,16 @@ namespace Atena.Delegate.Lesson
     {
        public void Create(EUDigitalWalletAction personalWallet) 
        {
-            Console.WriteLine(personalWallet());          
-       }
+            // Console.WriteLine(personalWallet());
+
+
+            // If the delegate invocation includes output parameters or a return value,
+            // their final value will come from the invocation of the last delegate in the list.
+            foreach (EUDigitalWalletAction myDlgt in personalWallet.GetInvocationList())
+            {
+                Console.WriteLine(myDlgt());
+            }
+        }
        public void Delete(Action<string> notificationAction) 
        {
             Console.ForegroundColor = ConsoleColor.Red; 
@@ -65,5 +159,4 @@ namespace Atena.Delegate.Lesson
             Console.ResetColor(); 
        }
     }
-    
-}
+
